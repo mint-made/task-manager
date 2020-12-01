f1be2f33d788974a996658e1c3e38853e16aab2a
mint-made <thomaskupai@gmail.com>
2020-12-01 16:24:54 +0000

    mongodb initialised

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,6 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -13,6 +12,45 @@ MongoClient.connect(
     if (error) {
       return console.log('Unable to connect to the database');
     }
-    console.log('Connected correctly to the database');
+
+    const db = client.db(databaseName);
+
+    // db.collection('users')
+    //   .updateOne(
+    //     {
+    //       _id: new ObjectID('5fc51074e15e532ea129ab77'),
+    //     },
+    //     {
+    //       $inc: { age: -100 },
+    //     }
+    //   )
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    // db.collection('users')
+    //   .deleteMany({
+    //     age: 27,
+    //   })
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    db.collection('tasks')
+      .deleteOne({
+        description: 'Make onion bhajis',
+      })
+      .then((results) => {
+        console.log(results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 );
