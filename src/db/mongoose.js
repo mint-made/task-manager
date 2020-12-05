0006f3153563462ea9cc3961396ff232bcf95609
mint-made <thomaskupai@gmail.com>
2020-12-05 13:27:32 +0000

    mongoose validation and data sanitisation added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -9,49 +10,76 @@ const User = mongoose.model('User', {
   name: {
     type: String,
     required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    },
   },
   age: {
     type: Number,
+    default: 0,
     validate(value) {
       if (value < 0) {
         throw new Error('Age must be a positive number');
       }
     },
   },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 6,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "password"');
+      }
+    },
+  },
 });
 
-const me = new User({
-  name: 'Jeff',
-  age: -1,
-});
-
-me.save()
-  .then(() => {
-    console.log(me);
-  })
-  .catch((error) => {
-    console.log('Error!', error);
-  });
-
-// const Task = mongoose.model('Task', {
-//   description: {
-//     type: String,
-//   },
-//   completed: {
-//     type: Boolean,
-//   },
+// const me = new User({
+//   name: '   Thomas   ',
+//   email: 'THOMASs@mail.com    ',
+//   password: '  password123   ',
 // });
 
-// const task = new Task({
-//   description: 'Clean the kitchen surfaces',
-//   completed: false,
-// });
-
-// task
-//   .save()
-//   .then((task) => {
-//     console.log(task);
+// me.save()
+//   .then(() => {
+//     console.log(me);
 //   })
 //   .catch((error) => {
-//     console.log(error);
+//     console.log('Error!', error);
 //   });
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const task = new Task({
+  description: 'Clean the kitchen surfaces  ',
+});
+
+task
+  .save()
+  .then((task) => {
+    console.log(task);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
