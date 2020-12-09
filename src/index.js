da2020f94f001e6f806ea4705ce60c38bedecee1
mint-made <thomaskupai@gmail.com>
2020-12-09 21:20:07 +0000

    Login http end point created

--- src/index.js
+++ src/index.js
@@ -7,10 +7,23 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
 app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
+
+const jwt = require('jsonwebtoken');
+
+const myFunction = async () => {
+  const token = jwt.sign({ _id: 'abc' }, 'thisismynewcourse', {
+    expiresIn: '7 days',
+  });
+  console.log(token);
+
+  const data = jwt.verify(token, 'thisismynewcourse');
+  console.log(data);
+};
+
+myFunction();
--- src/routers/user.js
+++ src/routers/user.js
@@ -18,6 +18,18 @@ router.post('/users', async (req, res) => {
   }
 });
 
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 router.get('/users', async (req, res) => {
   try {
     const users = await User.find({});
@@ -62,10 +74,9 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await User.findById(_id);
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
     if (!user) {
       return res.status(404).send({ error: 'No user found' });
     }
