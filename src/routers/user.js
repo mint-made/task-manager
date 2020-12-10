ba1452952bc75541839a0cd93ac2a3019c8776a8
mint-made <thomaskupai@gmail.com>
2020-12-10 10:58:07 +0000

    require authentication to edit user details

--- src/routers/user.js
+++ src/routers/user.js
@@ -60,30 +60,21 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.patch('/users/:id', async (req, res) => {
-  const _id = req.params.id;
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'email', 'password', 'age'];
   const isValidOpperation = updates.every((update) =>
     allowedUpdates.includes(update)
   );
+
   if (!isValidOpperation) {
     return res.status(400).send({ error: 'Update not permitted' });
   }
 
-  if (!ObjectId.isValid(_id)) {
-    console.log('invalid id');
-    return res.status(404).send({ error: 'Invalid ID' });
-  }
-
   try {
-    const user = await User.findById(_id);
-    updates.forEach((update) => (user[update] = req.body[update]));
-    await user.save();
-    if (!user) {
-      return res.status(404).send({ error: 'No user found' });
-    }
-    res.send(user);
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send(req.user);
   } catch (e) {
     res.status(400).send(e);
   }
