c18df7f3ccd89d4e9ed771d486e7d3d753ad7d36
mint-made <thomaskupai@gmail.com>
2020-12-23 19:10:27 +0000

    user signup test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,1 +1,25 @@
 const request = require('supertest');
+const app = require('../src/app');
+const User = require('../src/models/user');
+
+const userOne = {
+  name: 'Mike',
+  email: 'Mike@example.com',
+  password: 'braman45345',
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
+
+test('Should signup a new user', async () => {
+  await request(app)
+    .post('/users')
+    .send({
+      name: 'Thomas',
+      email: 'kupaithomas@gmail.com',
+      password: 'Mypass345237',
+    })
+    .expect(201);
+});
