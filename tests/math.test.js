ff39d070cde17779dd844bb29b163ff762e68154
mint-made <thomaskupai@gmail.com>
2020-12-23 13:31:11 +0000

    restructure app.js and index.js

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,8 @@
-const { calcTip } = require('../src/math');
+const {
+  calcTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit,
+} = require('../src/math');
 
 test('should calculate total with tip', () => {
   const total = calcTip(10, 0.3);
@@ -9,4 +13,21 @@ test('should calculate total with tip', () => {
 test('should calculate total with default tip', () => {
   const total = calcTip(10);
   expect(total).toBe(12.5);
+});
+
+test('should convert 32 F to 0 C', () => {
+  const degreesC = fahrenheitToCelsius(32);
+  expect(degreesC).toBe(0);
+});
+
+test('Should convert 0 C to 32 F', () => {
+  const degreesF = celsiusToFahrenheit(0);
+  expect(degreesF).toBe(32);
+});
+
+test('Async test demo', (done) => {
+  setTimeout(() => {
+    expect(1).toBe(1);
+    done();
+  }, 2000);
 });
